58b78f9c71da9ab06bf500d7fc5fd58adebe073a
Marc Bryan <mbryan@invidi.com>
2024-05-27 11:44:54 -0600

    Add index to publish script

--- publish.sh
+++ publish.sh
@@ -15,7 +15,7 @@ cp ~/localgit/hockeydraft/$year/index.html playoffs/$year.html
 sed -i -E 's;\.\./css;css;g' playoffs/$year.html
 
 git --no-pager diff && \
-    git add playoffs/$year.html && \
+    git add playoffs/$year.html playoffs/index.html && \
     git commit -m "$msg" && \
     git push
 
